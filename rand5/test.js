0f8413b49e9846dea01a964454b74f7cfed8ac36
Peter Todd Decker <ptdecker@mac.com>
2015-04-09 12:06:01 -0500

    Adjusted generic helper Rand() function to a more generic solution.

--- rand5/test.js
+++ rand5/test.js
@@ -8,7 +8,9 @@
  * You have a function rand7() that generates a random integer from 1 to 7. Use it to write a function rand5()
  * that generates a random integer from 1 to 5. rand7() returns each integer with equal probability. rand5() must
  * also return each integer with equal probability.
- */
+ *
+ *
+ * /
 
 "use strict";
 
